53638e82fbe51c8573d2c43b2fd17b39ab207899
Hadrian Wegrzynowski <hadrian@hawski.com>
2017-09-09 10:31:15 +0200

    Fix bootstrap issues.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,23 +1,24 @@
 #!/bin/sh
 set -e
 
-wget  "https://repo.voidlinux.eu/static/xbps-static-latest.x86_64-musl.tar.xz"
-mkdir xbps
-tar xf -C xbps xbps-static-latest.x86_64-musl.tar.xz
+wget  -N "https://repo.voidlinux.eu/static/xbps-static-latest.x86_64-musl.tar.xz"
+mkdir -p xbps/usr/share/xbps.d
+tar xf xbps-static-latest.x86_64-musl.tar.xz -C xbps
 cp 00-repository-main.conf xbps/usr/share/xbps.d
 
-git clone "https://github.com/voidlinux/void-packages.git"
+(cd void-packages && git pull origin master) ||
+	git clone "https://github.com/voidlinux/void-packages.git"
 (cd void-packages
-export PATH="xbps/usr/bin:$PATH"
+export PATH="$PWD/../xbps/usr/bin:$PATH"
+which xbps-install
 ./xbps-src binary-bootstrap x86_64-musl
-patch PATCH PATCH
+git apply ../void-packages.patch
 cp ../void-packages.conf etc/conf
 ./xbps-src pkg libGL
 ./xbps-src pkg gtk+3
 ./xbps-src pkg xbps-triggers
 )
 
-cp -r rootfs{-next,}
 mkdir -p rootfs/{config,dev,proc,sys,tmp,usr/{bin,lib},var}
 (cd rootfs;
 ln -s bin usr/bin
@@ -26,7 +27,7 @@ ln -s lib usr/lib
 
 gcc -O2 builder.c -o builder
 
-./builder ./rootfs-xbps-install base-system xbps-triggers dash \
+./rootfs-xbps-install -A -S xbps-triggers base-system dash \
 	xorg-minimal xorg-input-drivers xorg-video-drivers xorg-fonts \
 	firefox
 # busybox?
