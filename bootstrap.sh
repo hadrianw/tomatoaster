d2e8aab440c98835b46a3e33b6ff2db5c07ea3a9
Hadrian Węgrzynowski <hadrian@hawski.com>
2019-06-09 23:08:10 +0200

    Configure packages in chroot in rootfs.
    As from kernel version 4.14 the v3 capabilities xattr landed it is
    no longer needed to patch setcap.
    
    By using chroot to configure packages patching of the xbps-triggers is
    no longer needed. That also makes patching of void-packges and building
    of several packages also unnecessary.
    
    Builder tool is replaced by unshare-chroot (which probably should be renamed)
    it is used for masking chgrp and chown binaries as builder before,
    but also to run a chroot under a unshare with user mapped as root.
    Void's xbps-uunshare seems not enough to do this.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,57 +6,46 @@ mkdir -p xbps/usr/share/xbps.d
 tar xf xbps-static-latest.x86_64-musl.tar.xz -C xbps
 cp 00-repository-main.conf xbps/usr/share/xbps.d
 
+export PATH="$PWD/xbps/usr/bin:$PATH"
+
 (cd void-packages && git pull origin master) ||
 	git clone --depth 1 "https://github.com/void-linux/void-packages.git"
 (cd void-packages
-export PATH="$PWD/../xbps/usr/bin:$PATH"
 which xbps-install
 ./xbps-src binary-bootstrap x86_64-musl
-git apply ../void-packages.patch
+#git apply ../void-packages.patch
 cp ../void-packages.conf etc/conf
-for p in base-files dbus dracut eudev gtk+3 libGL mdocml xbps-triggers; do
+for p in gtk+3; do
 	./xbps-src pkg "$p"
 done
 )
 
 mkdir -p rootfs
 (cd rootfs;
-mkdir -p dev proc rw sys tmp usr/bin usr/lib var/db/xbps/keys/
+mkdir -p dev proc rw sys tmp usr/bin usr/lib var/db/xbps/keys/ build-tmp
 ln -s usr/bin bin
 ln -s usr/lib lib
 )
 cp xbps/var/db/xbps/keys/* rootfs/var/db/xbps/keys/
 
-# When v3 of security.capability xattr will be live in kernel near me
-# patching of setcap and mksquashfs will no longer be necessary
+# From kernel 4.14 the v3 of security.capability xattr is available
 # details: https://lwn.net/Articles/689169/
-git clone --depth 1 git://git.kernel.org/pub/scm/linux/kernel/git/morgan/libcap.git
-(cd libcap
-git apply ../libcap.patch
-)
-./build-chroot.ch libcap sh <<EOF
-xbps-install -y perl
-cd /build-chroot/libcap
-make
-EOF
-
+(cd squashfs-tools && git pull origin master) ||
 git clone --depth 1 https://git.code.sf.net/p/squashfs/code squashfs-tools
 (cd squashfs-tools
 git apply ../squashfs-tools.patch
-)
-./build-chroot.ch squashfs-tools sh <<EOF
-xbps-install -y zlib-devel
-cd /build-chroot/squashfs-tools/squashfs-tools
+cd squashfs-tools
 make
-EOF
+)
 
-
-gcc -O2 builder.c -o builder
-
-./rootfs-xbps-install -y -S xbps-triggers base-system dracut dash \
+gcc -O2 unshare-chroot.c -o unshare-chroot
+./rootfs-xbps-install --yes --unpack-only --sync \
+	base-system attr-progs \
 	xorg-minimal xorg-input-drivers xorg-video-drivers xorg-fonts \
-	firefox
-# busybox?
-# libreoffice?
+	firefox libreoffice
+# FIXME: dracut: mknod failed
+./rootfs-xbps-reconfigure --all
 
 cp fstab rootfs/etc
+
+# TODO: mksquashfs rootfs
---
+++ unshare-chroot.c
@@ -0,0 +1,167 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <sched.h>
+#include <sys/mount.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+static void
+usage()
+{
+	fputs("usage: ./unshare-chroot [-d SRC DST] [-b SRC DST] [-m NEW_ROOT] -- CMD [ARGS]\n", stderr);
+}
+
+int
+main(int argc, char *argv[])
+{
+	int i;
+	uid_t uid;
+	uid_t gid;
+	char *src;
+	char *dst;
+
+	if(argc < 3) {
+		usage();
+		fputs("unshare-chroot: not enough arguments\n", stderr);
+		return -1;
+	}
+
+	uid = getuid();
+	gid = getgid();
+
+	if(unshare(CLONE_NEWUSER|CLONE_NEWNS) == -1) {
+		perror("unshare");
+		return -1;
+	}
+
+	for(i=1; i < argc && strcmp(argv[i], "--"); ) {
+		if(!strcmp(argv[i], "-d") && i + 2 < argc) {
+			src = argv[i+1];
+			dst = argv[i+2];
+			if(chdir(src) == -1) {
+				fprintf(stderr,
+					"uunshare-mask: chdir %s failed: %s\n",
+					src, strerror(errno)
+				);
+			}
+			src = ".";
+			if(mount(src, dst, NULL, MS_BIND|MS_REC|MS_PRIVATE, NULL) == -1) {
+				fprintf(stderr,
+					"uunshare-mask: bind mount %s on %s failed: %s\n",
+					src, dst, strerror(errno)
+				);
+				return -1;
+			}
+			i += 3;
+		} else if(!strcmp(argv[i], "-b") && i + 2 < argc) {
+			src = argv[i+1];
+			dst = argv[i+2];
+			if(mount(src, dst, NULL, MS_BIND|MS_REC|MS_PRIVATE, NULL) == -1) {
+				fprintf(stderr,
+					"uunshare-mask: bind mount %s on %s failed: %s\n",
+					src, dst, strerror(errno)
+				);
+				return -1;
+			}
+			i += 3;
+		} else if(!strcmp(argv[i], "-m") && i + 1 < argc) {
+			src = argv[i+1];
+			dst = "/";
+			if(chdir(src) == -1) {
+				fprintf(stderr,
+					"uunshare-mask: chdir %s failed: %s\n",
+					src, strerror(errno)
+				);
+			}
+			if(mount(".", ".", NULL, MS_BIND|MS_PRIVATE, NULL) == -1) {
+				fprintf(stderr,
+					"uunshare-mask: bind mount . on . failed: %s\n",
+					strerror(errno)
+				);
+				return -1;
+			}
+			if(mount(src, dst, NULL, MS_MOVE, NULL) == -1) {
+				fprintf(stderr,
+					"uunshare-mask: bind mount %s on %s failed: %s\n",
+					src, dst, strerror(errno)
+				);
+				return -1;
+			}
+			if(chroot(".") == -1) {
+				fprintf(stderr,
+					"uunshare-mask: chroot . failed: %s\n",
+					strerror(errno)
+				);
+			}
+			i += 2;
+		} else {
+			usage();
+			return -1;
+		}
+	}
+
+	if(i+1 >= argc) {
+		usage();
+		fputs("expected -- argument and the command the least", stderr);
+		return -1;
+	}
+
+	if(strcmp(argv[i], "--")) {
+		usage();
+		fputs("expected -- argument", stderr);
+		return -1;
+	}
+
+	FILE *file;
+	int len;
+
+	file = fopen("/proc/self/uid_map", "wb");
+	if(!file) {
+		perror("fopen uid_map");
+		return -1;
+	}
+	len = fprintf(file, "0 %d 1\n", uid);
+	if(len < 0) {
+		perror("write uid_map");
+		return -1;
+	}
+	fclose(file);
+
+	file = fopen("/proc/self/setgroups", "wb");
+	if(!file) {
+		perror("fopen setgroups");
+		return -1;
+	}
+	len = fputs("deny", file);
+	if(len < 0) {
+		perror("write setgroups");
+		return -1;
+	}
+	fclose(file);
+
+	file = fopen("/proc/self/gid_map", "wb");
+	if(!file) {
+		perror("fopen gid_map");
+		return -1;
+	}
+	len = fprintf(file, "0 %d 1\n", gid);
+	if(len < 0) {
+		perror("write gid_map");
+		return -1;
+	}
+	fclose(file);
+
+	/*
+	if(unshare(CLONE_NEWNS) == -1) {
+		perror("unshare ns");
+		return -1;
+	}
+	*/
+
+	argv += i + 1;
+	execvp(argv[0], argv);
+	perror("execvp");
+	return -1;
+}
