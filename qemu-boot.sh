bb7d8cb823ce2cf507f114d54ccef90d46896b1c
Hadrian Węgrzynowski <hadrian@hawski.com>
2019-07-06 12:54:57 +0200

    Launch qemu with kvm and virtio for gpu.

--- qemu-boot.sh
+++ qemu-boot.sh
@@ -1,6 +1,8 @@
 qemu-system-x86_64 \
 	-drive file="$1",index=0,format=raw \
 	-drive file=tmtstr-rw.img,index=1,format=raw \
+	-enable-kvm -cpu host \
+	-vga virtio -display sdl,gl=on \
 	-no-reboot -serial stdio -m 2G \
 	-kernel rootfs/boot/vmlinuz-* -initrd rootfs/boot/initramfs-* \
 	-append "root=/dev/sda panic=1 PATH=/usr/bin console=ttyS0"
