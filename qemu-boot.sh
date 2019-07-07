4a7c7231c0884762fe20ad5089b200af1c29ddf7
Hadrian Węgrzynowski <hadrian@hawski.com>
2019-07-07 22:44:17 +0200

    Use access-order for qemu testing.

--- qemu-boot.sh
+++ qemu-boot.sh
@@ -5,4 +5,4 @@ qemu-system-x86_64 \
 	-vga virtio -display sdl,gl=on \
 	-no-reboot -serial stdio -m 2G \
 	-kernel rootfs/boot/vmlinuz-* -initrd rootfs/boot/initramfs-* \
-	-append "root=/dev/sda panic=1 PATH=/usr/bin console=ttyS0"
+	-append "root=/dev/sda panic=1 PATH=/usr/bin console=ttyS0 init=/sbin/access-order"
